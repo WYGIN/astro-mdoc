515c4902c9e6a6d8e3e1cdcc8e60dcf5d74597df
WYGIN <wygininc@gmail.com>
2023-10-21 12:49:42 +0000

    adapter to vercel
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- sample/astro-mdoc/astro.config.mjs
+++ sample/astro-mdoc/astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdoc from "astro-mdoc";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdoc()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
