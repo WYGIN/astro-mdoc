204bca2e180b565ef21ac1cd1f3266d00d9fa8f0
WYGIN <wygininc@gmail.com>
2023-10-21 12:35:45 +0000

    changed dapter from vercel to cloudflare
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- sample/astro-mdoc/astro.config.mjs
+++ sample/astro-mdoc/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdoc from "astro-mdoc";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdoc()],
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare()
 });
